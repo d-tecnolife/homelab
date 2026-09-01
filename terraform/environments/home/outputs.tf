2daf95f75bfb678088d3f948cb0ea38165a7d4fd
Russell Gallinera <dtecnolife@proton.me>
2026-09-01 15:11:21 -0500

    chore: remove old home environment

--- terraform/environments/home/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "core_vm_id" {
-  description = "VMID assigned to core."
-  value       = proxmox_virtual_environment_vm.core.vm_id
-}
-
-output "core_ipv4_addresses" {
-  description = "IPv4 addresses reported by the QEMU guest agent after startup."
-  value       = proxmox_virtual_environment_vm.core.ipv4_addresses
-}
